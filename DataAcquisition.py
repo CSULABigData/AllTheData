0273f548954ced4660dd9060e250988f5bb1662a
Wklein <mushroomhermit@gmail.com>
2015-05-16 21:08:58 -0700

    Updates

--- DataAcquisition.py
+++ DataAcquisition.py
@@ -3,60 +3,61 @@
 # 
 # @author: Alex
 # '''
-# import oauth2 as oauth
-# import json
+import oauth2 as oauth
+import json
+import pymongo
+import datetime
 # 
-# 
-# keyword = raw_input("Enter a keyword:")
-# # Set the API endpoint 
-# 
-# token = oauth.Token(key="", secret="")
-# consumer = oauth.Consumer(key="", secret="")
-# searchURL = 'https://api.twitter.com/1.1/search/tweets.json?q=%23' + str(keyword)
-# client = oauth.Client(consumer, token)
-# 
-# response, data = client.request(searchURL)
-# 
-# tweets = json.loads(data)
-# print(json.dumps(tweets, sort_keys=True))
 
 #Variables that contains the user credentials to access Twitter API 
 
 #Import the necessary methods from tweepy library
-# from tweepy.streaming import StreamListener
-# from tweepy import OAuthHandler
-# from tweepy import Stream
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
 
-# access_token = "ENTER YOUR ACCESS TOKEN"
-# access_token_secret = "ENTER YOUR ACCESS TOKEN SECRET"
-# consumer_key = "ENTER YOUR API KEY"
-# consumer_secret = "ENTER YOUR API SECRET"
-# 
-# 
-# #This is a basic listener that just prints received tweets to stdout.
-# class StdOutListener(StreamListener):
-# 
-#     def on_data(self, data):
-#         print data
-#         return True
-# 
-#     def on_error(self, status):
-#         print status
-# 
-# 
-# if __name__ == '__main__':
-# 
-#     #This handles Twitter authetification and the connection to Twitter Streaming API
-#     l = StdOutListener()
-#     auth = OAuthHandler(consumer_key, consumer_secret)
-#     auth.set_access_token(access_token, access_token_secret)
-#     stream = Stream(auth, l)
-# 
-#     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-#     stream.filter(track=['python', 'javascript', 'ruby'])
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
+ 
+client = pymongo.MongoClient()
+db = client.tweetcounts
+storyName = "Linux"
+trackWord = ["Linux", "Ubuntu"]
 
-import sys
-print sys.path
+
+ #This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+ 
+	def on_data(self, data):
+		year = datetime.datetime.now().year
+		month = datetime.datetime.now().month
+		day = datetime.datetime.now().day
+		hour = datetime.datetime.now().hour
+		if(db.counts.count({'name':trackWord, 'year': year, 'month': month, 'day': day, 'hour': hour}) > 0):
+			count = db.counts.find_one({'name':trackWord, 'year': year, 'month': month, 'day': day, 'hour': hour}).get('count')
+			db.counts.update({'name':trackWord, 'year': year, 'month': month, 'day': day, 'hour': hour},
+				{'name':trackWord, 'year': year, 'month': month, 'day': day, 'hour': hour, 'count':count + 1})
+		else:
+			db.counts.insert({'name':trackWord, 'year': year, 'month': month, 'day': day, 'hour': hour, 'count':1})
+		print db.counts.find_one({'name':trackWord, 'year': year, 'month': month, 'day': day, 'hour': hour}).get('count')
+		return True
+ 
+	def on_error(self, status):
+		print status	
+ 
+if __name__ == '__main__':
+ 
+	#This handles Twitter authetification and the connection to Twitter Streaming API
+	l = StdOutListener()
+	auth = OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	stream = Stream(auth, l)
+
+	#This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+	stream.filter(track=trackWord)
+
 
 
 
