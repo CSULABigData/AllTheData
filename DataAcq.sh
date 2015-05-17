b30efa8ae82b5c0eb49a32c7c0d84a12d1c07fa9
Wklein <mushroomhermit@gmail.com>
2015-05-16 21:10:06 -0700

    Shell Script to run all the acquisition programs

---
+++ DataAcq.sh
@@ -0,0 +1,7 @@
+gnome-terminal -x python BradyAcquisition.py
+gnome-terminal -x python GrammaAcquisition.py
+gnome-terminal -x python LibyaAcquisition.py
+gnome-terminal -x python NebraskaPrisonAcquisition.py
+gnome-terminal -x python ShellArcticAcquisition.py
+gnome-terminal -x python TyphoidAcquisition.py
+gnome-terminal -x python ZimmermanAcquisition.py
